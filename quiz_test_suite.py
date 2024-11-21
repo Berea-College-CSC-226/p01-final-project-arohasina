e4e9a51ff327f608fad3bc0e82c07e3253baba4b
Arohasina <ravoahanginiainaa@berea.edu>
2024-11-20 21:05:17 -0500

    created stubbed out quiz suite for testing

---
+++ quiz_test_suite.py
@@ -0,0 +1,40 @@
+#################################################################################
+# Author: Arohasina Ravohanginiaina
+# Username: Arohasina
+#
+# Assignment: p01
+# Purpose: Interactive learning flashcards and quizzes for users to study French vocabulary.
+#
+# Google Doc Link: https://docs.google.com/document/d/1jGpdXDdU7qafmN9_lEAigKttqIAKwALNgQ69IkVdbLk/edit?tab=t.0
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+from inspect import getframeinfo, stack
+from quiz import *
+
+def unittest(did_pass):
+    """
+    Print the result of a unit test.
+    :param did_pass: a boolean representing the test
+    :return: None
+    """
+
+    caller = getframeinfo(stack()[1][0])
+    linenum = caller.lineno
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+
+def funky_functions_suite():
+    """
+    The test_suite function utilizes the testit() function,
+    and is designed to test the quizz() class.
+
+    :return: None
+    """
