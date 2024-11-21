bb99727c94f8b9098f96eab0e7fad18782fc25bb
Arohasina <ravoahanginiainaa@berea.edu>
2024-11-20 21:05:35 -0500

    created stubbed out quiz class

---
+++ quiz.py
@@ -0,0 +1,36 @@
+#################################################################################
+# Author: Arohasina Ravohanginiaina
+# Username: Arohasina
+#
+# Assignment: p01
+# Purpose: Interactive learning flashcards and quizzes for users to study French vocabulary.
+#
+# Google Doc Link: https://docs.google.com/document/d/1jGpdXDdU7qafmN9_lEAigKttqIAKwALNgQ69IkVdbLk/edit?tab=t.0
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+
+class Quiz:
+    def __init__(self, question, answers,correct_answer):
+        """Initializes a Quiz instance with a question and a list of answers and the correct answer"""
+        pass
+
+    def display(self):
+        """Display the quiz question and answer choices """
+        pass
+
+    def check_answer(self, user_answer):
+        """Compares the user’s answer to the correct answer, returning True if correct, otherwise False."""
+        pass
+
+    def give_feedback(self, is_correct):
+        """Provides feedback to the user based on whether their answer was correct or not."""
+
+    def user_choice(self):
+        pass
+
+    def start_quiz(self):
+        pass
