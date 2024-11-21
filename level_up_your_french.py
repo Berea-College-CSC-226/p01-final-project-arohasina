d1e11521232095b835f09cd8710aca1b9fb1a6ba
Arohasina <ravoahanginiainaa@berea.edu>
2024-11-20 21:06:23 -0500

    created stubbed out python program for the main program

---
+++ level_up_your_french.py
@@ -0,0 +1,39 @@
+#################################################################################
+# Author: Arohasina Ravohanginiaina
+# Username: Arohasina
+#
+# Assignment: p01
+# Purpose: Interactive learning flashcards and quizzes for users to study French vocabulary.
+#
+# Google Doc Link: https://docs.google.com/document/d/1jGpdXDdU7qafmN9_lEAigKttqIAKwALNgQ69IkVdbLk/edit?tab=t.0
+#
+#################################################################################
+# Acknowledgements:
+#
+#
+#################################################################################
+from flashcard import *
+from quiz import *
+
+class MainProgram:
+    def __init__(self):
+        """
+        MainProgram class for handling the program logic.
+        """
+        pass
+
+    def run(self):
+        pass
+
+def main():
+    """
+    Starts the Level Up Your French program.
+
+    :return: None
+    """
+    program= MainProgram
+    program.run()
+    pass
+
+if __name__ == "__main__":
+    main()
